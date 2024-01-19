1b3f83da7385dca784e3348bac5e992566b96b83
mbaibatyr <murat_b@mail.ru>
2024-01-19 21:07:00 +0600

    --

--- MyWinForm/MyForm.Designer.cs
+++ MyWinForm/MyForm.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -41,15 +43,36 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(3, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(132, 409);
+            this.listBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(334, 78);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(208, 28);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Load data";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // MyForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
             this.Name = "MyForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "MyForm";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MyForm_FormClosed);
             this.ResumeLayout(false);
 
         }
@@ -57,5 +80,7 @@
         #endregion
 
         private Button button1;
+        private ListBox listBox1;
+        private Button button2;
     }
 }
--- MyWinForm/MyForm.cs
+++ MyWinForm/MyForm.cs
@@ -15,23 +15,38 @@ namespace MyWinForm
     public partial class MyForm : Form
     {
         static IConfigurationRoot config;
+        SqlConnection db;
         public MyForm()
         {
             InitializeComponent();
             config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            db = new SqlConnection(config["conStr"]);
+            db.Open();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            using (SqlConnection db = new SqlConnection(config["conStr"]))
+            try
             {
-                db.Open();
-                if (db.State == ConnectionState.Open)
-                    MessageBox.Show("Connected");
-                else                    
-                    MessageBox.Show("Not connected");
-                db.Close();
+                using (SqlConnection db = new SqlConnection(config["conStr"]))
+                {
+                    db.Open();
+                    if (db.State == ConnectionState.Open)
+                        MessageBox.Show("Connected");
+                    else
+                        MessageBox.Show("Not connected");
+                    db.Close();
+                }
             }
+            catch (Exception err)
+            {                
+                MessageBox.Show(err.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void MyForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            db.Close();
         }
     }
 }
