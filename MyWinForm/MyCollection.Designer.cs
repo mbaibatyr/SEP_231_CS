28751c4d62f938200bec5f0c961d98639eb37a0a
mbaibatyr <murat_b@mail.ru>
2024-01-23 21:33:39 +0600

    --

---
+++ MyWinForm/MyCollection.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace MyWinForm
+{
+    partial class MyCollection
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            listBox1 = new ListBox();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(353, 21);
+            button1.Name = "button1";
+            button1.Size = new Size(230, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Создание List";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.Dock = DockStyle.Left;
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(0, 0);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(179, 450);
+            listBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(353, 73);
+            button2.Name = "button2";
+            button2.Size = new Size(230, 23);
+            button2.TabIndex = 2;
+            button2.Text = "Создание ArrayList";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // MyCollection
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(listBox1);
+            Controls.Add(button1);
+            Name = "MyCollection";
+            Text = "MyCollection";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private ListBox listBox1;
+        private Button button2;
+    }
+}
---
+++ MyWinForm/MyCollection.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyWinForm
+{
+    public partial class MyCollection : Form
+    {
+        public MyCollection()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<string> list = new List<string>();
+            for (int i = 0; i < 20; i++)
+            {
+                list.Add(i.ToString());
+            }
+            foreach (string item in list)
+            {
+                listBox1.Items.Add(item);
+            }            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ArrayList list = new ArrayList();
+            list.Add("123");
+            list.Add(123);
+            list.Add(new object());
+            list.
+        }
+    }
+}
