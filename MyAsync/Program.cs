696b679438ed676678db5a54718869b36775582e
mbaibatyr <murat_b@mail.ru>
2024-02-13 21:41:07 +0600

    --

---
+++ MyAsync/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace MyAsync
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Start");
+
+            Parallel.Invoke(
+                () =>
+                {
+                    Console.WriteLine("hello step 1");
+                },
+                () =>
+                {
+                    Console.WriteLine("hello step 2");
+                },
+                () =>
+                {
+                    Console.WriteLine("hello step 3");
+                }
+            );
+
+            //Test1.MyPrint(1, 10);
+            //Test1.MyPrint(10, 20);
+
+            //var t1 = Task.Run(() => Test1.MyPrint(1, 10));
+            //var t2 = Task.Run(() => Test1.MyPrint(10, 20));
+            //var t3 = Task.Run(() => Test1.MyPrint(100, 120));
+
+            //await Task.WhenAny(t1, t2, t3);
+
+            Console.WriteLine("Finish");
+        }
+    }
+
+    class Test1
+    {
+        public static void MyPrint(int min, int max)
+        {
+            for (int i = min; i <= max; i++)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(200);
+            }
+
+        }
+    }
+}
